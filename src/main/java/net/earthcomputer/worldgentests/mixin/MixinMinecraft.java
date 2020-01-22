0b80ac8ef3b981381a1283b9ab2415d08988ae2e
Joe <burtonjae@hotmail.co.uk>
2020-01-22 12:38:40 +0000

    Implement basic example mod

---
+++ src/main/java/net/earthcomputer/worldgentests/mixin/MixinMinecraft.java
@@ -0,0 +1,17 @@
+package net.earthcomputer.worldgentests.mixin;
+
+import net.minecraft.client.Minecraft;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Minecraft.class)
+public class MixinMinecraft {
+
+	@Inject(method = "run", at = @At("HEAD"))
+	private void onRun(CallbackInfo ci) {
+		System.out.println("Hello from a mixin!");
+	}
+
+}
